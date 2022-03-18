83e08162619d185c46db4d6efb4736950c7060c3
vishnu-vasudev <vishnuviva97@gmail.com>
2022-03-18 19:51:47 +0530

    Updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn worldrentals.wsgi --log-file -
--- worldrentals/settings.py
+++ worldrentals/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
